ef19aa81b35813c46d54c6e009ab5453d70e7a17
Bamieh <ahmadbamieh@gmail.com>
2017-05-10 14:29:58 +0300

    update sanity checks

--- test/sanity-checks.test.js
+++ test/sanity-checks.test.js
@@ -1,10 +1,18 @@
-import { expect } from 'chai';
 import processor from '../src'
 import styleObject from './fixture/styleObject'
 import processorConfigs from './fixture/processorConfigs'
 
 
-describe('Basic Processor Behavior', () => {
+describe('Sanity Checks:: Processor', () => {
+  it('is exported as default', () => {
+    // if you change the processor to stop exporting a default, this will trigger.
+    expect(processor).to.equal(require('../src').default);
+  })
+
+  it('is a function', () => {
+    expect(processor).to.be.a('function');
+  })
+
   it('returns an object', () => {
     const processed = processor(styleObject, processorConfigs);
     expect(processed).to.be.an('object');
